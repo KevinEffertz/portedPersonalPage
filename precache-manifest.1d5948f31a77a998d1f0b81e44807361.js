b50452fd7f1dff6a8264255db8604ac1bdd240e8
Kevin Effertz <kevin.effertz@gmail.com>
2020-06-05 16:14:03 -0500

    Updates

---
+++ precache-manifest.1d5948f31a77a998d1f0b81e44807361.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1583236cb49a8ca8fc03ae7e0890cd86",
+    "url": "/portedPersonalPage/index.html"
+  },
+  {
+    "revision": "21a6197c933dda61bfe3",
+    "url": "/portedPersonalPage/static/css/main.061eeaa2.chunk.css"
+  },
+  {
+    "revision": "5fd42270a3a2a13ff5a0",
+    "url": "/portedPersonalPage/static/js/2.9240a0cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portedPersonalPage/static/js/2.9240a0cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a6197c933dda61bfe3",
+    "url": "/portedPersonalPage/static/js/main.4fc2730a.chunk.js"
+  },
+  {
+    "revision": "971732252301cbbd5daf",
+    "url": "/portedPersonalPage/static/js/runtime-main.c56608e6.js"
+  },
+  {
+    "revision": "7f56472bc557a42384dd2f54d0d00fb3",
+    "url": "/portedPersonalPage/static/media/DSC5345_250X395.7f56472b.jpg"
+  },
+  {
+    "revision": "09b39c43e79f08c3bd6652704181741e",
+    "url": "/portedPersonalPage/static/media/DSC5381_1584x396.09b39c43.jpg"
+  },
+  {
+    "revision": "5f64fb4347f27bc7c956cd957b57cc64",
+    "url": "/portedPersonalPage/static/media/DigitalTransformation-Web.5f64fb43.png"
+  },
+  {
+    "revision": "8db000933c641841b55058264a17128f",
+    "url": "/portedPersonalPage/static/media/ExtractTransformLoad.8db00093.png"
+  },
+  {
+    "revision": "203644865caac9eb52698a72ebfd68f7",
+    "url": "/portedPersonalPage/static/media/IT Support.20364486.png"
+  },
+  {
+    "revision": "2b5979a7767e8b5def96dc3accc5f92f",
+    "url": "/portedPersonalPage/static/media/UW-Stout_20190622_152336.2b5979a7.jpg"
+  },
+  {
+    "revision": "cb04f5be8602a3a1ea00996a9f730242",
+    "url": "/portedPersonalPage/static/media/University-of-StThomas_DSC5602.cb04f5be.JPG"
+  },
+  {
+    "revision": "7fb27183286f2ece3de0e73aa3f34ff9",
+    "url": "/portedPersonalPage/static/media/WI-statute-TOC.7fb27183.png"
+  }
+]);
